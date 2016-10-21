9847bfda31bf37f2e1d4e601c6d08128942336f0
Nicolas Motte <lingusinpg@gmail.com>
2016-10-21 10:57:22 +0200

    Improve output

--- plot.sh
+++ plot.sh
@@ -5,27 +5,31 @@ ALL_SERVERS="ns6737901.ip-164-132-166.eu ns6737902.ip-164-132-166.eu ns6737903.i
 # Start dstat
 for SERVER in $BIG_SERVERS
 do
-ssh -o "StrictHostKeyChecking no" root@$SERVER << EOF
-nohup dstat --noheaders -t -n -N eth1 -d -D sdd,sdc -c -m --output plot.dat 5 > /dev/null 2>&1&
+echo "# Starting dstat on ${SERVER}"
+ssh -o "StrictHostKeyChecking no" root@$SERVER << EOF > /dev/null 2>&1&
+nohup dstat --noheaders -t -n -N eth1 -d -D sdd,sdc -c -m -y --output plot.dat 5 > /dev/null 2>&1&
 ps aux | grep dstat | grep plot.dat | awk '{print \$2}' > dstat.pid
 EOF
 done
 for SERVER in $SMALL_SERVERS
 do
-ssh -o "StrictHostKeyChecking no" root@$SERVER << EOF
-nohup dstat --noheaders -t -n -N eth1 -d -D sda,sdb -c -m --output plot.dat 5 > /dev/null 2>&1&
+echo "# Starting dstat on ${SERVER}"
+ssh -o "StrictHostKeyChecking no" root@$SERVER << EOF > /dev/null 2>&1&
+nohup dstat --noheaders -t -n -N eth1 -d -D sda,sdb -c -m -y --output plot.dat 5 > /dev/null 2>&1&
 ps aux | grep dstat | grep plot.dat | awk '{print \$2}' > dstat.pid
 EOF
 done
 
 
 # Wait for test
+echo "# Monitoring for $1 seconds..."
 sleep $1
 
 # Stop dstat
 for SERVER in $ALL_SERVERS
 do
-ssh -o "StrictHostKeyChecking no" root@$SERVER << EOF
+echo "# Stopping dstat on ${SERVER}"
+ssh -o "StrictHostKeyChecking no" root@$SERVER << EOF > /dev/null 2>&1
 cat dstat.pid | xargs kill
 rm -f dstat.pid
 EOF
@@ -34,8 +38,9 @@ done
 # Gather stats
 for SERVER in $ALL_SERVERS
 do
-scp -o "StrictHostKeyChecking no" root@$SERVER:./plot.dat ./${SERVER}.dat
-ssh -o "StrictHostKeyChecking no" root@$SERVER << EOF
+echo "# Retrieving stats from ${SERVER}"
+scp -o "StrictHostKeyChecking no" root@$SERVER:./plot.dat ./${SERVER}.dat > /dev/null 2>&1
+ssh -o "StrictHostKeyChecking no" root@$SERVER << EOF > /dev/null 2>&1
 rm -f plot.dat
 EOF
 done
@@ -48,8 +53,9 @@ done
 
 rm -f plot.gnu
 # Create GNU Plot file
+echo "# Generating gnuplot configuration file"
 cat << EOF >> plot.gnu
-set terminal png size 4096,3200 enhanced font "Helvetica,20"
+set terminal png size 5120,3200 enhanced font "Helvetica,20"
 set output 'output.png'
 set datafile separator ","
 set key outside left
@@ -64,7 +70,7 @@ set style line 4 lt 1 lc rgb "#0000A0" lw 2 pt 8 ps 1
 set style line 5 lt 1 lc rgb "#D0D000" lw 2 pt 13 ps 1
 set style line 6 lt 1 lc rgb "#00D0D0" lw 2 pt 12 ps 1
 set style line 7 lt 1 lc rgb "#B200B2" lw 2 pt 5 ps 1
-set multiplot layout 13,4 rowsfirst
+set multiplot layout 13,5 rowsfirst
 EOF
 
 for SERVER in $ALL_SERVERS
@@ -88,8 +94,14 @@ plot "${SERVER}.dat" u (\$8+\$9+\$10+\$11) w filledcurves x1 t "usr", \
 # Memory
 set label 1 'Memory ${SERVER}' at graph 0.2,1.1 font ',8'
 set format y '%.0s%cB'
-plot "${SERVER}.dat" u (\$14+\$15) w filledcurves x1 t "used", \
-     "${SERVER}.dat" u (\$14) w filledcurves x1 t "free"
+plot "${SERVER}.dat" u (\$14+\$15+\$16+\$17) w filledcurves x1 t "used", \
+     "${SERVER}.dat" u (\$15+\$16+\$17) w filledcurves x1 t "buf", \
+     "${SERVER}.dat" u (\$16+\$17) w filledcurves x1 t "cache", \
+     "${SERVER}.dat" u (\$17) w filledcurves x1 t "free"
+# System stats
+set label 1 'System stats ${SERVER}' at graph 0.2,1.1 font ',8'
+unset format
+plot "${SERVER}.dat" u 2 w lp ls 1 t "Csw"
 EOF
 done
 
@@ -97,4 +109,5 @@ cat << EOF >> plot.gnu
 unset multiplot
 EOF
 
+echo "# Dumping graphs in output.png"
 gnuplot -p plot.gnu &
